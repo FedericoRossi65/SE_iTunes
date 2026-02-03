2546f5cdb95324697f2a4af295de60403511ec4c
Federico <frossimiglio@gmail.com>
2026-02-03 10:22:38 +0100

    Prima parte ok

--- UI/controller.py
+++ UI/controller.py
@@ -11,19 +11,19 @@ class Controller:
     def handle_crea_grafo(self, e):
         self._view.lista_visualizzazione_1.clean()
         durata = int(self._view.txt_durata.value)
-        self._model.build_graph(durata)
-        nodi,archi = self._model.graph_det()
-        self._view.lista_visualizzazione_1.controls.append(ft.Text(f'I nodi sono: {nodi}, e archi : {archi}'))
-        album = self._model.load_album(int(self._view.txt_durata.value))
-        for a in album:
-            self._view.dd_album.options.append(ft.dropdown.Option(key=a.id,text=a))
+        if durata > 0:
+            self._model.build_graph(durata)
+            nodi,archi = self._model.graph_det()
+            self._view.lista_visualizzazione_1.controls.append(ft.Text(f'I nodi sono: {nodi}, e archi : {archi}'))
+            album = self._model.load_album(int(self._view.txt_durata.value))
+            for a in album:
+                self._view.dd_album.options.append(ft.dropdown.Option(key=a.id,text=a))
+        else:
+            self._view.show_alert(f'Inserisci un valore intero')
         self._view.update()
     def get_selected_album(self, e):
         """ Handler per gestire la selezione dell'album dal dropdown """""
         self.selezione = self._model.id_map[int(self._view.dd_album.value)]
-
-
-
 
     def handle_analisi_comp(self, e):
         """ Handler per gestire l'analisi della componente connessa """""
